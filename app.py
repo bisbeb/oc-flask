0b3bffe66b5815d8600ea7a69c04ef37aa96cecc
beb <beb@linux-w9om.home>
2019-02-23 21:07:52 +0100

    Debug disabled

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ def hello_world(name=None):
     return render_template('hello.html', name=name)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=False)
 
